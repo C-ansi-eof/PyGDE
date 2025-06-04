5d0f2bba7b4c84ae3132b51961ff1825e6f9e7a9
C.Olmos <carlos.olmos111@gmail.com>
2025-06-04 09:26:08 +0200

    set clean folder

--- matlab1/leeclave.m
+++
@@ -1,7 +0,0 @@
-function[lectura]=leeclave(fide,fids,clave)
-lectura=fscanf(fide,'%s',1);
-if ~strcmp(lectura,clave)
-fprintf(fids,'\n\n***ERROR*** Se ha leido la clave: " %s ", en lugar de: " %s "\n\n',lectura,clave);
-close('all');
-error('clave incorrecta');
-end
--- matlab1/leernomb.m
+++
@@ -1,9 +0,0 @@
-function[Nombre]=leernomb(fid,fids,cnombre,mensaje)
-[As,count]=fscanf(fid,'%s',1); % Leer el nombre.
-      nblancos=cnombre-max(size(As)); % Admitir un máximo de cnombre caracteres.
-      Nombre=[blanks(nblancos) As];
-      if nblancos<0
-         Nombre=As(:,1:cnombre);
-         nblancos=0;
-         fprintf(fids,'Se trunca el nombre %s a los %d primeros caracteres: %s\n\n',mensaje,cnombre,Nombre);
-      end
